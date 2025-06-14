511ebf2ca82ce6b1f99d0262ee5944a36c77da45
KR3RULIE <mjt.kr3rulie@gmail.com>
2025-06-14 16:47:41 -0300

    baje el footer y agregue las dependencias

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import Formulario from "./components/Formulario";
+
 function App() {
   return (
     <>
-      <h1>Ejercicio 05</h1>
+      <main>
+        <section className="container">
+          <h1 className="text-center">Ejercicio 05</h1>
+          <Formulario />
+        </section>
+      </main>
+      <footer className="bg-dark text-light text-center py-2 fs-5">
+        <p>
+          By <a href="https://github.com/KR3RULIE">KR3RULIE</a>
+        </p>
+        <p>&copy; Todos los derechos reservaos</p>
+      </footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
